62020354a265538645b5958ccbca5c2fdb65c3b4
Florence Dugot-Duhamel <merlin.dugot@gmail.com>
2017-09-29 23:04:10 +0900

    puts #OK

--- checker.c
+++ checker.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
 void test();
+void ft_puts(char *str);
+void test3();
+
+void dbg_int(int x)
+{
+	printf("=> %d\n", x);
+}
+
+void dbg_char(char c)
+{
+	printf("=> %c\n", c);
+}
+
+void dbg_str(char *str)
+{
+	printf("=> %s\n", str);
+}
 
 int main(void)
 {
-	printf("ASM LIBFT CHECKER\n");
-	test();
+	char str[] = "this is a long test with a lot of words in it\n";
+	printf("//////////////////////////////////\n");
+	printf("                                  \n");
+	printf("         LIBFT ASM CHECKER        \n");
+	printf("                                  \n");
+	printf("//////////////////////////////////\n\n");
+	ft_puts(str);
+	ft_puts("don't touch this\n");
+	ft_puts("");
 	return 0;
 }
--- sources/ft_puts.s
+++ sources/ft_puts.s
@@ -1,18 +1,32 @@
 section .data
-hello:
-	.string db "Hello World!", 10
-	.len equ $ - hello.string
 
 section .text
-	global start
-	global _test
-	extern _printf
+	global _ft_puts
 
-_test:
+_ft_puts:
 	push rbp
 	mov rbp, rsp
 	sub rsp, 16
-	lea rdi, [rel hello.string]
-	call _printf
+	mov rsi, rdi
+	mov rdi, 1
+	call _get_len
+	mov rax, 0x2000004
+	syscall
 	leave
 	ret
+
+_get_len:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	mov rdx, 0
+_loop:
+	cmp byte [rsi+rdx], 0
+	je _return
+	jne _inc
+_return:
+	leave
+	ret
+_inc:
+	add rdx, 1
+	jmp _loop
